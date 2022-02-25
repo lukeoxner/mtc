3c4414658f39303d657c3817d4735a78b85986aa
Luke <68408655+lukeoxner@users.noreply.github.com>
2022-02-25 11:29:58 -0700

    add 2022 track dates

--- client/src/pages/Schedule.js
+++ client/src/pages/Schedule.js
@@ -1,45 +1,45 @@
-import React from "react";
+import React from 'react';
 import {
 	Button,
 	makeStyles,
 	useTheme,
 	Container,
 	Grid,
-} from "@material-ui/core";
-import { Link } from "react-router-dom";
-import Background from "../images/group-edit.jpg";
-import Typography from "@material-ui/core/Typography";
-import MediaCard from "../components/MediaCard";
+} from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import Background from '../images/group-edit.jpg';
+import Typography from '@material-ui/core/Typography';
+import MediaCard from '../components/MediaCard';
 
 const useStyles = makeStyles((theme) => ({
 	mainDiv: {
-		position: "relative",
+		position: 'relative',
 		backgroundImage: `linear-gradient(to left, rgba(000, 000, 000, 0.8), rgba(000, 000, 000, 0.80)), url(${Background})`,
-		width: "100",
-		height: "100vh",
-		backgroundRepeat: "no-repeat",
-		backgroundSize: "cover",
-		backgroundPosition: "center center",
-		backgroundAttachment: "fixed",
-		overflow: "hidden",
+		width: '100',
+		// height: '100vh',
+		backgroundRepeat: 'no-repeat',
+		backgroundSize: 'cover',
+		backgroundPosition: 'center center',
+		backgroundAttachment: 'fixed',
+		overflow: 'hidden',
 	},
 	root: {
 		minWidth: 275,
-		maxHeight: "90",
-		marginTop: "7%",
+		maxHeight: '90',
+		marginTop: '7%',
 		backgroundColor: `rgba(0,0,0,.0)`,
-		color: "white",
-		overflow: "hidden",
+		color: 'white',
+		overflow: 'hidden',
 	},
 	hero: {
-		fontSize: "2.3rem",
-		fontWeight: "600",
-		textShadow: "2px 2px #000000",
-		textAlign: "center",
-		marginTop: "40px",
-		marginBottom: "40px",
-		[theme.breakpoints.down("sm")]: {
-			fontSize: "2.0rem",
+		fontSize: '2.3rem',
+		fontWeight: '600',
+		textShadow: '2px 2px #000000',
+		textAlign: 'center',
+		marginTop: '40px',
+		marginBottom: '40px',
+		[theme.breakpoints.down('sm')]: {
+			fontSize: '2.0rem',
 		},
 	},
 }));
@@ -55,23 +55,24 @@ function Schedule() {
 					<Typography className={classes.hero}>Upcoming Events</Typography>
 					<Grid
 						container
-						direction="row"
-						justifyContent="center"
-						alignItems="center"
+						direction='row'
+						justifyContent='center'
+						alignItems='center'
 					>
 						<Grid item sm={8} lg={6}>
-							<MediaCard />
-
-							{/* <Details
-								id={result.id}
-								key={result.id}
-								name={result.name}
-								street={result.street}
-								city={result.city}
-								state={result.state}
-								website={result.website_url}
-								type={result.brewery_type}
-							/> */}
+							<MediaCard date='March 14, 2022' />
+						</Grid>
+						<Grid item sm={8} lg={6}>
+							<MediaCard date='May 16, 2022' />
+						</Grid>
+						<Grid item sm={8} lg={6}>
+							<MediaCard date='June 13, 2022' />
+						</Grid>
+						<Grid item sm={8} lg={6}>
+							<MediaCard date='July 18, 2022' />
+						</Grid>
+						<Grid item sm={8} lg={6}>
+							<MediaCard date='September 12, 2022' />
 						</Grid>
 					</Grid>
 				</Container>
@@ -81,3 +82,14 @@ function Schedule() {
 }
 
 export default Schedule;
+
+// {/* <Details
+// 								id={result.id}
+// 								key={result.id}
+// 								name={result.name}
+// 								street={result.street}
+// 								city={result.city}
+// 								state={result.state}
+// 								website={result.website_url}
+// 								type={result.brewery_type}
+// 							/> */}
