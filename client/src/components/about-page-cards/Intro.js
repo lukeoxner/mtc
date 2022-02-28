f10e003add73f9f12e1ab6330c959fe6ebb1ed0f
Luke <68408655+lukeoxner@users.noreply.github.com>
2022-02-28 13:42:32 -0700

    work on about nav

---
+++ client/src/components/about-page-cards/Intro.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Intro() {
+	return (
+		<div>
+			<h2>
+				McPherson Track Center offers you the chance to enjoy your high
+				performance car in a safe, controlled environment on the race track.
+			</h2>
+			<br />
+			<h4>
+				Whether you're brand new to high performance driving, or a seasoned
+				track day veteran - you'll have a blast our events.
+			</h4>
+		</div>
+	);
+}
+
+export default Intro;
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,61 +1,62 @@
-import React from "react";
+import React from 'react';
 import {
 	Button,
 	makeStyles,
 	useTheme,
 	Container,
 	Grid,
-} from "@material-ui/core";
-import { Link } from "react-router-dom";
-import Background from "../images/two-cars-back.jpg";
-import Typography from "@material-ui/core/Typography";
-import MediaCard from "../components/MediaCard";
-import AboutNav from "../components/AboutNav";
+} from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import Background from '../images/two-cars-back.jpg';
+import Typography from '@material-ui/core/Typography';
+import MediaCard from '../components/MediaCard';
+import AboutNav from '../components/AboutNav';
+import IntroCard from '../components/about-page-cards/Intro';
 
 const useStyles = makeStyles((theme) => ({
 	mainDiv: {
-		position: "relative",
-		backgroundColor: "#fff",
-		width: "100",
-		height: "100vh",
-		backgroundRepeat: "no-repeat",
-		backgroundSize: "cover",
-		backgroundPosition: "center center",
-		backgroundAttachment: "fixed",
-		overflow: "hidden",
+		position: 'relative',
+		backgroundColor: '#fff',
+		width: '100',
+		height: '100vh',
+		backgroundRepeat: 'no-repeat',
+		backgroundSize: 'cover',
+		backgroundPosition: 'center center',
+		backgroundAttachment: 'fixed',
+		overflow: 'hidden',
 	},
 	root: {
 		minWidth: 275,
 		// maxHeight: "90",
-		marginTop: "7%",
+		marginTop: '7%',
 		backgroundColor: `rgba(0,0,0,.0)`,
-		overflow: "hidden",
+		overflow: 'hidden',
 	},
 	hero: {
-		marginTop: "50px",
-		marginBottom: "20px",
+		marginTop: '50px',
+		marginBottom: '20px',
 		// paddingTop: "50px",
-		height: "400px",
-		width: "100",
+		height: '400px',
+		width: '100',
 		backgroundImage: `linear-gradient(to left, rgba(000, 000, 000, 0.6), rgba(000, 000, 000, 0.70)), url(${Background})`,
-		backgroundRepeat: "no-repeat",
-		backgroundSize: "cover",
-		backgroundPosition: "50% 80%",
-		backgroundAttachment: "fixed",
+		backgroundRepeat: 'no-repeat',
+		backgroundSize: 'cover',
+		backgroundPosition: '50% 80%',
+		backgroundAttachment: 'fixed',
 	},
 	heroText: {
-		fontSize: "2.5rem",
-		fontWeight: "600",
-		color: "#fff",
-		textAlign: "center",
-		[theme.breakpoints.down("sm")]: {
-			fontSize: "2.0rem",
+		fontSize: '2.5rem',
+		fontWeight: '600',
+		color: '#fff',
+		textAlign: 'center',
+		[theme.breakpoints.down('sm')]: {
+			fontSize: '2.0rem',
 		},
-		paddingTop: "70px",
+		paddingTop: '70px',
 	},
 	gridSection: {
 		// padding: "20px",
-		color: "#444",
+		color: '#444',
 		// justifyContent: "center",
 		// alignItems: "center",
 	},
@@ -64,6 +65,8 @@ const useStyles = makeStyles((theme) => ({
 function About() {
 	const classes = useStyles();
 	const theme = useTheme();
+
+	let activeCard = 'IntroCard';
 
 	return (
 		<>
@@ -74,24 +77,22 @@ function About() {
 				<Container className={classes.root}>
 					<Grid
 						container
-						direction="row"
-						justifyContent="center"
+						direction='row'
+						justifyContent='center'
 						// alignItems="center"
 					>
 						<Grid className={classes.gridSection} item sm={10} lg={3}>
 							<AboutNav />
 						</Grid>
 						<Grid className={classes.gridSection} item sm={10} lg={9}>
-							<h2>
-								McPherson Track Center offers you the chance to enjoy your high
-								performance car in a safe, controlled environment on the race
-								track.
-							</h2>
-							<br />
-							<h4>
-								Whether you're brand new to high performance driving, or a
-								seasoned track day veteran - you'll have a blast our events.
-							</h4>
+							{(() => {
+								switch (activeCard) {
+									case 'IntroCard':
+										return <IntroCard />;
+									default:
+										return null;
+								}
+							})()}
 						</Grid>
 					</Grid>
 				</Container>
