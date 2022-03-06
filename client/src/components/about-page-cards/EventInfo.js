5ca5192e1a23c99d4cb5edfe7b27ff7a8c9cfa84
Luke <68408655+lukeoxner@users.noreply.github.com>
2022-03-06 11:41:27 -0700

    event info placeholder text

--- client/src/components/about-page-cards/EventInfo.js
+++ client/src/components/about-page-cards/EventInfo.js
@@ -5,18 +5,38 @@ import {
 	useTheme,
 	Container,
 	Grid,
+	Typography,
 } from '@material-ui/core';
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+	heading: {
+		textAlign: 'center',
+		marginBottom: '15px',
+	},
+	subHeading: {
+		textAlign: 'center',
+		marginTop: '15px',
+	},
+}));
 
 function EventInfo() {
 	const classes = useStyles();
 	const theme = useTheme();
 
 	return (
-		<div>
-			<h2>Event Info Page</h2>
-		</div>
+		<>
+			<Grid container direction='row' justifyContent='center'>
+				<Grid item xs={12} className={classes.heading}>
+					<Typography variant='h4'>Event Info</Typography>
+				</Grid>
+				<Grid item xs={10}>
+					<br />
+					<Typography className={classes.heading}>
+						Here you will find info about our event format - coming soon!
+					</Typography>
+				</Grid>
+			</Grid>
+		</>
 	);
 }
 
